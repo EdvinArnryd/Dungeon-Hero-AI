989015399ea3481d32b11a4105c647ddf65f8532
unknown <edvin.arn@hotmail.com>
2024-09-27 21:14:27 +0200

    Cleaned up code

--- Assignment/Assets/Scripts/Game/Edvin_Arnryd/Edvin_Arnryd_Controller.cs
+++ Assignment/Assets/Scripts/Game/Edvin_Arnryd/Edvin_Arnryd_Controller.cs
@@ -10,42 +10,55 @@ using static UnityEngine.GraphicsBuffer;
 /// <summary>
 /// Replace the FirstName and LastName with your name and last name :)
 /// </summary>
+///
+///
+/// Things to add:
+/// Only pick up health if not full health
+/// Never attack or kick
+/// Always move
+/// Move around traps so that evenmies kill themselves
 namespace Edvin_Arnryd
 {
     public class Edvin_Arnryd_Controller : HeroController
     {
         public override ControllerAction Think()
         {
-			EnemyController enemy = EnemyController.AllEnemies.Find(e => IsNeighbor(e));
-            if (enemy != null)
-            {
-                return new Action_Attack(this, enemy);
-            }
-
-            Heart heart = Heart.AllHearts.Count > 0 ? Heart.AllHearts[0] : null;
+            
+            // Find the closest heart
             Heart closestHeart = Heart.AllHearts.Count > 0 ? Heart.AllHearts[0] : null;
             
-            
-            if (heart != null)
+            if (closestHeart != null)
             {
-                GraphAlgorithms.Path aPath = Dungeon.Instance.GetShortestPath(this, this.Node, heart.Node);
+                GraphAlgorithms.Path aPath = Dungeon.Instance.GetShortestPath(this, this.Node, closestHeart.Node);
                 GraphAlgorithms.Path shortestPath = aPath;
-                foreach (var eachHeart in Heart.AllHearts)
+                foreach (var heart in Heart.AllHearts)
                 {
-                    GraphAlgorithms.Path path = Dungeon.Instance.GetShortestPath(this, this.Node, eachHeart.Node);
+                    GraphAlgorithms.Path path = Dungeon.Instance.GetShortestPath(this, this.Node, heart.Node);
                     if (path.Count < shortestPath.Count)
                     {
                         shortestPath = path;
-                        closestHeart = eachHeart;
+                        closestHeart = heart;
                     }
                 }
-            }
-            
-
-            if (closestHeart != null)
-            {
                 return new Action_MoveTowards(this, closestHeart.Node);
             }
+            else
+            {
+                // Move towards closest trap tile and circle around it
+            }
+            
+            EnemyController enemy = EnemyController.AllEnemies.Find(e => IsNeighbor(e));
+            if (enemy != null)
+            {
+                return new Action_Kick(this, enemy);
+            }
+            
+            /*
+            // Walk towards closest heart
+            if (closestHeart != null)
+            {
+                
+            }*/
             
             
             // if (heart != null)
